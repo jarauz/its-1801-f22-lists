d8c6af1f627c8a5375ed37fb8fee5a899c4c3e8c
Julio Arauz <jarauz@alumni.pitt.edu>
2022-11-10 15:16:38 +0000

    List access example

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ Check out the official docs here: https://docs.python.org/3/library/turtle.html
 import turtle
 
 casper = turtle.Turtle()
+listOfColors = [ "red", "green", "blue", "yellow"]
 
-for c in ['red', 'green', 'blue', 'yellow']:
+for c in listOfColors:
     casper.color(c)
     casper.forward(75)
     casper.left(90)
 
-# The structure [0, 1, 2, 3] in Python is called a list
 # The structure ['red', 'green', 'blue', 'yellow'] is also a list
 
-for c in [0, 1, 2, 3]:
-  print("The value of c in this iteration is " + str(c))
+# Print just the first element of listOfColors to the console
+
+# Print just the second element of listOfColors to the console
+
+# Print just the third element of listOfColors to the console
+
+# Print just the fourth element of listOfColors to the console
