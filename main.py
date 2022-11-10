4ad25fb16b4e15761578723a52ae598f1c91685e
Julio Arauz <jarauz@alumni.pitt.edu>
2022-11-10 15:34:46 +0000

    Access individual elements of a list and range of elements

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ for c in listOfColors:
 # The structure ['red', 'green', 'blue', 'yellow'] is also a list
 
 # Print just the first element of listOfColors to the console
-print("First")
+print(listOfColors[0])
 # Print just the second element of listOfColors to the console
-
+print(listOfColors[1])
 # Print just the third element of listOfColors to the console
-
+print(listOfColors[2])
 # Print just the fourth element of listOfColors to the console
+print(listOfColors[3])
+# Print from element 1 to element 2
+print(listOfColors[1:3]) 
+# Python will print from index 1 to 2 (3 is not included)
